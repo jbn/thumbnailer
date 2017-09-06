5a6588bdd8a2b79381071ea89d99b163c566a3ce
John B Nelson <jbn@artifexdeus.com>
2017-09-06 10:22:30 -0700

    FIX bug in concurrency

--- thumbnailer.go
+++ thumbnailer.go
@@ -1,10 +1,8 @@
-// Creates thumbnails of all images in a directory.
 package main
 
 import (
     "bytes"
-    "strings"
-    "path/filepath"
+    "errors"
     "flag"
     "fmt"
     "github.com/disintegration/gift"
@@ -13,11 +11,14 @@ import (
     "image/png"
     "io"
     "log"
-    "os"
-    "time"
-    "runtime"
-    "sync"
     "math/rand"
+    "os"
+    "path/filepath"
+    "runtime"
+    "strings"
+    "strconv"
+    "sync"
+    "time"
     _ "image/gif"
     _ "image/jpeg"
 )
@@ -26,18 +27,46 @@ import (
 
 var inputDir     = flag.String("i", "image_packs", "input directory")
 var outputDir    = flag.String("o", "image_thumbs", "output directory")
-var deduplicate  = flag.Bool("d", true, "skip duplicates")
+var deduplicate  = flag.Bool("n", true, "skip duplicates")
 var shufflePaths = flag.Bool("s", true, "shuffle image paths")
 var flipVertical = flag.Bool("f", true, "flip vertical")
 
+// This isn't a flag. But, it's populated based on flipVertical.
 var flipOps      = []bool{false}
+
+type dim_t [2]int
+
+func (p *dim_t) String() string {
+    return fmt.Sprintf("%d,%d", p[0], p[1])
+}
+
+func (p *dim_t) Set(raw string) error {
+    parts := strings.Split(raw, ",")
+    if len(parts) != 2 {
+        return errors.New("Dimensions argument expected string like `X,Y`")
+    }
+
+    for i, s := range parts {
+        v, err := strconv.ParseInt(s, 10, 32)
+        if err != nil {
+            return fmt.Errorf("%q not an integer in %q", v, raw)
+        }
+        p[i] = int(v)
+    }
+
+    return nil
+}
+
+// Default is the receptor field for VGG16.
+var thumbDim dim_t = dim_t{224, 224}
+
+func init() {
+    flag.Var(&thumbDim, "d", "Thumbnail Dimensions")
+}
 
 //=============================================================================
 
-var wg sync.WaitGroup
-
-var RECEPTOR_FIELD = []int{224, 224}
-
+// XXX: TODO: Allow for CLI anchor spec.
 var ANCHORINGS = map[string]gift.Anchor{
     "left": gift.LeftAnchor,
     "right": gift.RightAnchor,
@@ -46,18 +75,21 @@ var ANCHORINGS = map[string]gift.Anchor{
 
 //=============================================================================
 
-func readImage(path string) (image.Image, int64, error) {
+var wg sync.WaitGroup
+
+func readImage(path string) (img image.Image, checksum int64, err error) {
     fp, err := os.Open(path)
     defer fp.Close()
+
     if err != nil {
         return nil, -1, err
     }
 
     buf := bytes.NewBuffer(nil)
     io.Copy(buf, fp)
-    checksum := crc32.ChecksumIEEE(buf.Bytes())
+    checksum = int64(crc32.ChecksumIEEE(buf.Bytes()))
 
-    img, _, err := image.Decode(buf)
+    img, _, err = image.Decode(buf)
     if err != nil {
         return nil, -1, err
     }
@@ -72,34 +104,35 @@ func calcResizeBounds(src image.Image) (int, int) {
     x, y := bounds.Max.X, bounds.Max.Y
 
     if x < y {
-        s := RECEPTOR_FIELD[0] / x
-        return RECEPTOR_FIELD[0], y * s
+        s := thumbDim[0] / x
+        return thumbDim[0], y * s
     } else {
-        s := RECEPTOR_FIELD[1] / y
-        return x * s, RECEPTOR_FIELD[1]
+        s := thumbDim[1] / y
+        return x * s, thumbDim[1]
     }
 }
 
 func subImage(src image.Image) image.Image {
     x, y := calcResizeBounds(src)
+
     g := gift.New(gift.Resize(x, y, gift.LanczosResampling))
     dst := image.NewNRGBA(g.Bounds(src.Bounds()))
     g.Draw(dst, src)
+
     return dst
 }
 
 
-func createThumb(src image.Image, anchors map[string]gift.Anchor) map[string]image.Image {
+func createThumbs(src image.Image, anchors map[string]gift.Anchor) map[string]image.Image {
     thumbs := make(map[string]image.Image)
 
-    // Precalculate the sub-image.
     src = subImage(src)
 
     for k, anchor := range anchors {
         for _, flipped := range flipOps {
             outputName := k
 
-            filters := []gift.Filter{gift.CropToSize(RECEPTOR_FIELD[0], RECEPTOR_FIELD[1], anchor)}
+            filters := []gift.Filter{gift.CropToSize(thumbDim[0], thumbDim[1], anchor)}
             if flipped {
                 outputName += "_flipped"
                 filters = append(filters, gift.FlipHorizontal())
@@ -134,61 +167,79 @@ func saveThumb(filepath string, img image.Image) {
 // shuffling with deduplication, everything is loaded into memory first. That's 
 // not feasible for some datasets.
 
-var PROCESSORS = runtime.NumCPU() * 2
-var fileChan = make(chan string, 4*PROCESSORS)
+var nProcessors = runtime.NumCPU() * 2
 
+var filePaths = make(chan string, 4*nProcessors)
 
-func validFile(path string, info os.FileInfo) bool {
+var totalPaths = -1
+
+func isImageFile(path string, info os.FileInfo) bool {
     baseName := filepath.Base(path)
     return (baseName[0] != '.' && // No hidden files
             !info.IsDir() &&      // Real files
             info.Size() > 0)      // Not just markers
 }
 
-func gatherInputs(inputPath string) {
-    defer close(fileChan )
+func produceInputs(inputPath string) {
 
     if *shufflePaths {
         var paths []string
 
         // Gather all paths first.
         filepath.Walk(inputPath, func (path string, info os.FileInfo, err error) error {
-            if err == nil && validFile(path, info) {
+            if err == nil && isImageFile(path, info) {
                 paths = append(paths, path)
             }
             return err
         })
 
-        permutation := rand.Perm(len(paths))
-        for _, i := range permutation{
-            fileChan <- paths[i]
-        }
+        totalPaths = len(paths)
+
+        // Walk paths shuffled.
+        // Why? If you visit sequentially and use deplification, 
+        // files that are lexicographically earlier are less likely 
+        // to be deleted. It unbalances classes in a nonsensical way.
+        wg.Add(1)
+
+        go func() {
+            defer func() { close(filePaths); defer wg.Done() }()
+
+            for _, i := range rand.Perm(len(paths)) {
+                filePaths <- paths[i]
+            }
+        }()
 
     } else {
-        // Write to the channel ASAP.
-        filepath.Walk(inputPath, func (path string, info os.FileInfo, err error) error {
-            if err == nil && validFile(path, info) {
-                fileChan <- path
-            }
-            return err
-        })
+        wg.Add(1)
+        go func() {
+            defer func() { close(filePaths); defer wg.Done() }()
+            // Write to the channel ASAP.
+            filepath.Walk(inputPath, func (path string, info os.FileInfo, err error) error {
+                if err == nil && isImageFile(path, info) {
+                    filePaths <- path
+                }
+                return err
+            })
+        }()
     }
 }
 
-func outputPath(inputPath string, ensureDir bool) string {
+func outputPath(inputPath string, ensureDir bool) (string, error) {
     srcDir, srcName := filepath.Split(inputPath)
     parts := strings.Split(filepath.ToSlash(srcDir), "/")
     dstDir := *outputDir
 
     if len(parts) > 1 {
         dstDir = filepath.Join(dstDir, filepath.Join(parts[1:]...))
+    } else {
+        return "", fmt.Errorf("Can't split %s into parts", inputPath)
     }
 
     if ensureDir {
         os.MkdirAll(dstDir, os.ModePerm)
     }
 
-    return filepath.Join(dstDir, srcName)
+    return filepath.Join(dstDir, srcName), nil
 }
 
 var checksumMutex sync.Mutex
@@ -196,6 +247,8 @@ var checksumMutex sync.Mutex
 var checksums = make(map[int64]bool)
 
 func checkChecksum(checksum int64) bool {
+    // This should be better than a RWLock for most cases.
+    // Usually, you have only a few dupes.
     checksumMutex.Lock()
     defer checksumMutex.Unlock()
 
@@ -213,7 +266,7 @@ func processPath(inputFile string) {
 
     img, checksum, err := readImage(inputFile)
 
-    if *deduplicate && !checkChecksum(checksum) {
+    if *deduplicate && false && !checkChecksum(checksum) {
         fmt.Println("Skipping", inputFile)
         return
     }
@@ -222,11 +275,17 @@ func processPath(inputFile string) {
         log.Fatal(err)
     }
 
-    thumbs := createThumb(img, ANCHORINGS)
+    thumbs := createThumbs(img, ANCHORINGS)
 
-    outputFile := outputPath(inputFile, true)
+    outputFile, err := outputPath(inputFile, true)
+    if err != nil {
+        return // Just skip processing
+    }
+
     d, name := filepath.Split(outputFile)
-    name = name[:strings.Index(name, ".")]
+    if j := strings.Index(name, "."); j != -1 {
+        name = name[:j]
+    }
     for k, v := range thumbs {
         f_p := filepath.Join(d, name + "_" + k + ".png")
         fmt.Println("Saving", f_p)
@@ -236,14 +295,14 @@ func processPath(inputFile string) {
 }
 
 func consumer() {
-    for inputFile := range fileChan {
+    defer wg.Done()
+    for inputFile := range filePaths {
         processPath(inputFile)
     }
-    defer wg.Done()
 }
 
 func receiveInputs() {
-    for i := 0; i < PROCESSORS; i++ {
+    for i := 0; i < nProcessors; i++ {
         wg.Add(1)
         go consumer()
     }
@@ -259,7 +318,7 @@ func main() {
         flipOps = append(flipOps, true)
     }
 
-    gatherInputs(*inputDir)
+    produceInputs(*inputDir)
     receiveInputs()
 
     wg.Wait()
